8a443dbe13868107b1d6655820d17155b2705f7c
Anton Petrov <anton.petrov.swe@gmail.com>
2020-03-25 05:51:08 +0300

    Add tests for fetching exchange rate from storage, specify connection pool type

--- currency_converter_service/dependencies/database.py
+++ currency_converter_service/dependencies/database.py
@@ -1,7 +1,7 @@
 import aioredis
 
 
-async def create_connection_pool(uri: str):
+async def create_connection_pool(uri: str) -> aioredis.Redis:
     pool = await aioredis.create_redis_pool(uri)
 
     return pool
--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -1,10 +1,17 @@
 from typing import Dict, Iterator, Tuple
 
 import pytest
+from aioredis import Redis
 
 from currency_converter_service.currency import Currency
-from currency_converter_service.dependencies import preprocess
-from currency_converter_service.models import CurrencyExchangeRatesLoadRequest
+from currency_converter_service.dependencies import (
+    CurrencyExchangeRatesStorage,
+    preprocess,
+)
+from currency_converter_service.models import (
+    CurrencyExchangeRatesLoadRequest,
+    ExchangeRate,
+)
 
 
 @pytest.mark.parametrize(
@@ -77,3 +84,55 @@ def test_preprocess(
     loadable_exchange_rates: Iterator[Tuple[str, Dict[Currency, str]]],
 ) -> None:
     assert tuple(preprocess(raw_load_request)) == loadable_exchange_rates
+
+
+@pytest.mark.parametrize(
+    # fmt: off
+    (
+        "base_currency, quote_currency, "
+        "base_currency_storage_key, "
+        "stored_exchange_rates, "
+        "expected_exchange_rate"
+    ),
+    [
+        (
+            Currency.EUR, Currency.GBP,
+            "exchange-rates:EUR",
+            {
+                "RUB": '{"rate": "79.75", "last_updated": 1584989828}',
+                "GBP": '{"rate": "0.918316", "last_updated": 1584989828}',
+            },
+            ExchangeRate(rate="0.918316", last_updated=1584989828),
+        ),
+        (
+            Currency.EUR, Currency.GBP,
+            "exchange-rates:RUB",
+            {"RUB": '{"rate": "79.75", "last_updated": 1584989828}'},
+            None,
+        ),
+        (
+            Currency.EUR, Currency.GBP,
+            "exchange-rates:RUB", {},
+            None,
+        ),
+    ],
+    # fmt: on
+)
+@pytest.mark.asyncio
+async def test_fetch_exchange_rate(
+    database: Redis,
+    base_currency: Currency,
+    quote_currency: Currency,
+    base_currency_storage_key: str,
+    stored_exchange_rates: Tuple[str, Dict[Currency, str]],
+    expected_exchange_rate: ExchangeRate,
+) -> None:
+    if stored_exchange_rates:
+        await database.hmset_dict(base_currency_storage_key, stored_exchange_rates)
+
+    currency_exchange_rate_storage = CurrencyExchangeRatesStorage(database)
+    fetched_exchange_rate = await currency_exchange_rate_storage.fetch_exchange_rate(
+        base_currency, quote_currency
+    )
+
+    assert fetched_exchange_rate == expected_exchange_rate
